10f314c61320cec9859ac4ac3e014916e556c1ab
Tanmay Bagchi <tbagchi85@gmail.com>
2020-12-04 15:04:36 +0530

    sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
-# config = YAML.load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
-
-# redis_conn = { url: "redis://#{config[:host]}:#{config[:port]}/" }
-
-# Sidekiq.configure_server do |s|
-#   s.redis = redis_conn
-# end
-
-# Sidekiq.configure_client do |s|
-#   s.redis = redis_conn
-# end
+rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+rails_env = Rails.env || 'development'
+redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
+Sidekiq.configure_server do |config|
+ config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+end
+Sidekiq.configure_client do |config|
+ config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+end
