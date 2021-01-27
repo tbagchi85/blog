b6cd386669608a9f1a3169832c192075665607bc
Tanmay Bagchi <tbagchi85@gmail.com>
2021-01-27 08:48:05 +0530

    test 2

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths << Rails.root.join('app', 'workers')
+    # config.autoload_paths << Rails.root.join('app', 'workers')
 
 
     # Settings in config/environments/* take precedence over those specified here.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-	require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+#	require 'sidekiq/web'
+ # mount Sidekiq::Web => '/sidekiq'
 
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
