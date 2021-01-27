0fd09cff5529058d0920a41908e721c70bc6bfd8
Tanmay Bagchi <tbagchi85@gmail.com>
2021-01-27 10:50:36 +0530

    small chnages

--- app/workers/autocreate_blog_worker.rb
+++ app/workers/autocreate_blog_worker.rb
@@ -6,6 +6,6 @@ class AutocreateBlogWorker < ApplicationJob
 #  sidekiq_options retry: false
 
   def perform
-  	Article.create(title: "Title #{Time.current}", description: "Some description #{Article.last.id}")
+  	Article.create(title: "Title #{Time.current}", description: "Some description #{Article.last.id} - Version 1")
   end
 end
