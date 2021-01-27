db8a2a9a4475f11c5bcd72d80e05961c10d2af1d
Tanmay Bagchi <tbagchi85@gmail.com>
2021-01-27 10:09:04 +0530

    ggg

--- app/scripts/clock.rb
+++ app/scripts/clock.rb
@@ -1,5 +1,10 @@
+require 'clockwork'
+
 require './config/boot'
 require './config/environment'
+
+include Clockwork
+
 
 #TO BE ENABLED WHEN SFTP IN ENABLED
 # Clockwork.every(15.minutes, 'BlockTradePublisherJob') { BlockTradePublisherJob.perform_later }
